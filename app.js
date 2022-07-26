2b47187842959e68ee3b22d161e0b2fe49532582
Uriel Aguirre <uriiaguirre11@outlook.com>
2022-07-25 23:41:39 -0300

    adaptativo

--- app.js
+++ app.js
@@ -6,5 +6,7 @@ const port = 3030;
 app.use(express.static('public'));
 
 app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'home.html')))
+app.get('/registrer.html', (req,res) => res.sendFile(path.join(__dirname, 'views', 'registrer.html')))
+app.get('/login.html', (req,res) => res.sendFile(path.join(__dirname, 'views', 'login.html')))
 
 app.listen(port, ()=> console.log('Server running in http://localhost:' + port));
